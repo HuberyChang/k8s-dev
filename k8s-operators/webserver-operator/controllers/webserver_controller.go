554ed47710cab1aa86af51f6b362d865274433ea
HuberyChang <1632425633@qq.com>
2023-04-25 15:32:21 +0800

    add rbac configurations

--- k8s-operators/webserver-operator/controllers/webserver_controller.go
+++ k8s-operators/webserver-operator/controllers/webserver_controller.go
@@ -45,6 +45,9 @@ type WebServerReconciler struct {
 //+kubebuilder:rbac:groups=my.domain,resources=webservers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=my.domain,resources=webservers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=my.domain,resources=webservers/finalizers,verbs=update
+//+kubebuilder:rbac:groups=my.domain,resources=webservers/status,verbs=update;get;patch
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
